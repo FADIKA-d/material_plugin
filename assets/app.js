32773761fec7fa14161203890ffec7affb362536
Diaraye <d.fadika@gmail.com>
2024-03-03 16:26:36 +0100

    bad code need to be improved

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+import "./bootstrap.js";
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,15 +6,35 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 
-import './styles/app.css';
+import "./styles/app.css";
 
 // Initialisation des tooltips bootstrap
 (function () {
-    'use strict'
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-      new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-  })()
+  "use strict";
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+})();
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+$(document).ready(function () {
+  $("#material-list").DataTable({
+    language: {
+      url: "//cdn.datatables.net/plug-ins/2.0.1/i18n/fr-FR.json",
+    },
+    ajax: url_path,
+    processing: true,
+    serverSide: true,
+    columns: [
+      //  "id", "name", "quantity", "createdAt", "priceBeforeTax", "priceIncVAT"
+      { data: "id" },
+      { data: "name" },
+      { data: "quantity" },
+      { data: "priceBeforeTax" },
+      { data: "priceIncVAT" },
+      { data: "createdAt" },
+    ],
+  });
+});
--- src/Controller/MaterialController.php
+++ src/Controller/MaterialController.php
@@ -4,15 +4,35 @@ namespace App\Controller;
 
 use App\Repository\MaterialRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 class MaterialController extends AbstractController
 {
-    #[Route('/material', name: 'app_material')]
-    public function index(MaterialRepository $materialRepository): Response
+    #[Route('/material', name: 'app_material_index')]
+    public function index(MaterialRepository $materialRepository, Request $request): Response
     {
         $materials = $materialRepository->findAll();
+        // Tester l'existance d'une requête AJAX
+        if ($request->isXmlHttpRequest()) {
+
+
+            // Pb rencontré lors de la transformation de mon tableau d'objets en json : A circular reference has been detected when serializing the object of class "App\Entity\Material" (configured limit: 1)
+            // Le pb serait du à la présence de la relation 1:n avec l'entité VAT.
+
+            // solution trouvée sur internet https://stackoverflow.com/questions/59268438/a-circular-reference-has-been-detected-when-serializing-the-object-of-class-app
+            $defaultContext = [
+                AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function (object $object, string $format, array $context): string {
+                    return $object->getId();
+                },
+            ];
+
+
+            return $this->json(['data' => $materials], headers: ['Content-Type' => 'application/json;charset=UTF-8'], context: $defaultContext);
+        }
+
         return $this->render('material/index.html.twig', [
             'materials' => $materials,
         ]);
