794a9e87e49f7855417360ed0d31bd983edef7a2
Diaraye <d.fadika@gmail.com>
2024-03-03 10:48:18 +0100

    add sidebar

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,16 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+
 import './styles/app.css';
+
+// Initialisation des tooltips bootstrap
+(function () {
+    'use strict'
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+      new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  })()
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
